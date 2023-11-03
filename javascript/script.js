cf49cc3e7e3ff415cd2c4f2200504d4ad18bd1f4
Leandro Frigerio <leandro.frigerio@turbinekreuzberg.com>
2023-11-03 10:53:15 +0100

    Add contact form logic & icons

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,59 @@
-/* Here we will write our Javascript code */
+const form = document.getElementById('contact-form');
+const result = document.getElementById('form-result');
+
+//on submit, POST to web3Forms and get response
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+
+    const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
+
+    if (!hCaptcha) {
+        e.preventDefault();
+        alert("Please fill out captcha field")
+        return
+    }
+
+
+
+
+    result.innerHTML = "Please wait..."
+    //fetch result
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+        .then(async (response) => {//show response
+            let json = await response.json();
+            console.log(json);
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+                result.classList.add("alert-success");
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+                result.classList.add("alert-warning");
+
+            }
+        })
+        .catch(error => {//show error
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+            result.classList.add("alert-danger");
+
+        })
+        .then(function () {//reset form
+            form.reset();
+            //make response dissapear after 5 seconds
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 5000);
+        });
+});
